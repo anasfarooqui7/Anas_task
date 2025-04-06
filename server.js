431f0a84802758d244332e7d5169c54ab0f5c410
anasfarooqui7 <67817524+anasfarooqui7@users.noreply.github.com>
2025-04-06 15:49:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,4 +8,5 @@ app.use(express.json());
 app.use('/api', authRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
